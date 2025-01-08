daf03f589bdc1c533cd511ce0f6367af4d7f4da9
hlx <1198816371@qq.com>
2025-01-08 22:33:55 +0800

    createVnode h

---
+++ packages/runtime-core/src/vnode.ts
@@ -0,0 +1,31 @@
+import {isArray, isString, ShapeFlags} from "@vue/shared";
+
+export function isVnode(value) {
+    return !!(value && value.__v_isVnode);
+}
+
+export function createVnode(type, props, children = null) {
+    let shapeFlag =  isString(type) ? ShapeFlags.ELEMENT : 0;
+    const vnode = {
+        type,
+        props,
+        children,
+        el: null,
+        key: props?.key,
+        __v_isVnode: true,
+        shapeFlag,
+    }
+
+    if (children) {
+        let type = 0;
+        if (isArray(children)) {
+            type = ShapeFlags.ARRAY_CHILDREN;
+        } else {
+            children = String(children);
+            type = ShapeFlags.TEXT_CHILDREN;
+        }
+        vnode.shapeFlag |= type;
+    }
+
+    return vnode;
+}
